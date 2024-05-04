8ca5b1cfcb08a58d9bdb9d1393454088579185d5
Яна Попова <ynappva@gmail.com>
2024-05-05 02:16:43 +0300

    Csv added

--- src/test/java/ru/netology/NetologyJavaHW5/services/VacationCalcTest.java
+++ src/test/java/ru/netology/NetologyJavaHW5/services/VacationCalcTest.java
@@ -1,29 +1,32 @@
 package ru.netology.NetologyJavaHW5.services;
-
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class VacationCalcTest {
 
-    @Test
-    public void smallerThreshold() {
+    @ParameterizedTest
+    @CsvSource({
+            "3,10000,3000,20000",
+            "2,100000,60000,150000"
+    })
+    public void smallerThreshold(int expected,int income,int expenses,int threshold) {
         VacationCalc service = new VacationCalc();
 
-        int expected = 3;
-        int actual = service.calculate(10_000, 3000, 20_000);
+        //int expected = 3;
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
 
-
     }
 
-    @Test
+ /*   @Test
     public void biggerThreshold() {
         VacationCalc service = new VacationCalc();
 
         int expected = 2;
         int actual = service.calculate(100_000, 60_000, 150_000);
 
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual); */
     }
-}
+
